27b0b8d29b905c50c54b8af01e380462bc2625fb
Jeonjareinji <luthfirachman033@gmail.com>
2025-05-06 20:39:30 +0700

    pl;s

--- vite.config.ts
+++ vite.config.ts
@@ -27,9 +27,9 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./attached_assets")
     }
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(import.meta.dirname, "./client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "./dist/public"),
     emptyOutDir: true,
   },
 });
