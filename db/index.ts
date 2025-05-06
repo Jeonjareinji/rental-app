cd146baaa1a6bd526323df75cc453fbae201091d
Jeonjareinji <luthfirachman033@gmail.com>
2025-05-06 19:03:29 +0700

    force to string for env

--- db/index.ts
+++ db/index.ts
@@ -6,17 +6,24 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "@shared/schema";
 
 const pool = new Pool({
+  host: process.env.DB_HOST as string,
+  port: parseInt(process.env.DB_PORT as string),
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASSWORD as string, // Explicit string casting
+  database: process.env.DB_NAME as string,
+});
+
+console.log({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  password: typeof process.env.DB_PASSWORD, // Check type
+  database: process.env.DB_NAME
 });
 
 pool.query('SELECT 1')
   .then(() => console.log('✅ DB connected'))
   .catch((err) => console.error('❌ DB connection error:', err));
-
 
 export const db = drizzle(pool, { schema });
 export { pool };
