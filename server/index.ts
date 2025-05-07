720019bbdf53fd62317e1259eb96cd6ad3bf17fb
Jeonjareinji <luthfirachman033@gmail.com>
2025-05-07 09:39:52 +0700

    asdsadada

--- server/index.ts
+++ server/index.ts
@@ -26,8 +26,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
